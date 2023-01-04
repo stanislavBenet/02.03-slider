6b4a3ead7b3acb34eca580c924205d718528c183
stanislavBenet <stanislav.benet@gmail.com>
2023-01-04 23:21:31 +0200

    Add style for slider

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-"use strict"
+"use strict";
+
+const imagesDB = [
+   "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/20190502_Newhaven_to_Dieppe_Ferry_3.jpg/1200px-20190502_Newhaven_to_Dieppe_Ferry_3.jpg",
+   "https://upload.wikimedia.org/wikipedia/commons/b/b6/20190502_Newhaven_to_Dieppe_Ferry_3.jpg",
+   "https://rk.karelia.ru/wp-content/uploads/2016/05/More.jpg",
+];
+
+const img = document.querySelector(".slide>img");
+const [prevBtn, nextBtn] = document.querySelectorAll("slider-container>button");
